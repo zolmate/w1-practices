31443e05714e11653b2bff96e54664c5494d3ff9
zolmate <zolmate@gmail.com>
2022-04-01 17:26:12 +0200

    04.01. (Friday) update

---
+++ frontend/script.js
@@ -0,0 +1,25 @@
+console.log("Ez az első Javascriptem")
+/*
+// a stringeket (szöveg) idézőjelbe kell rakni
+"Ez itt egy szöveg"
+// a dupla és sima macskaköröm (quotemark) csak egysoros stringet (szöveget) enged meg
+'Ez itt egy másik szöveg'
+
+// backtickek közé rakott sortörés
+`Ez egy több
+soros string (szöveg)`
+
+// A számokat nem kell idézőjelbe tenni
+4
+
+128
+
+04
+
+1.13
+*/
+function myFirstFunction(){
+    console.log("Ez az első függvényem belsejében levő log")
+}
+
+myFirstFunction()
